02b13b4044fcef7216521b1e68b67bffb40b2bdf
sahmemmed hebibov <sahmemmedhebibov4@gmail.com>
2023-02-17 16:04:45 +0400

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Book
+    {
+        internal string name, Autname;
+        internal int PubDate, PageCount;
+
+
+
+        public override string ToString()
+        {
+            return $"{Autname} ''{name}'' {PubDate}";
+        }
+
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("nece kitab olacaq:");
+                int n = int.Parse(Console.ReadLine());
+                Book[] kitablar = new Book[n];
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    Book b = new Book();
+                    Console.Write($"{i + 1}.kitabin adi:");
+                    b.name = Console.ReadLine();
+                    Console.Write($"{i + 1}.muellifin adi:");
+                    b.Autname = Console.ReadLine();
+                    Console.Write($"{i + 1}.nesr edilme tarixi:");
+                    b.PubDate = int.Parse(Console.ReadLine());
+                    Console.Write($"{i + 1}.kitabin sehifesi:");
+                    b.PageCount = int.Parse(Console.ReadLine());
+                    kitablar[i] = b;
+                }
+                foreach (var item in kitablar)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+    }
+}
